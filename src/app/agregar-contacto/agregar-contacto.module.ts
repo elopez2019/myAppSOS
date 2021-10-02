0862e10a43f78730c71de5cad56dc5bcde2d0e48
emilie <49416341+elopez2019@users.noreply.github.com>
2021-10-02 12:40:10 -0500

    Formulario Contactos
    Se implmento el form al agregar contactos.

--- src/app/agregar-contacto/agregar-contacto.module.ts
+++ src/app/agregar-contacto/agregar-contacto.module.ts
@@ -1,20 +1,24 @@
 import { NgModule } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-
 import { AgregarContactoPageRoutingModule } from './agregar-contacto-routing.module';
-
 import { AgregarContactoPage } from './agregar-contacto.page';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+
+
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    AgregarContactoPageRoutingModule
+    AgregarContactoPageRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [AgregarContactoPage]
 })
-export class AgregarContactoPageModule {}
+export class AgregarContactoPageModule {
+}
--- src/app/agregar-contacto/agregar-contacto.page.ts
+++ src/app/agregar-contacto/agregar-contacto.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-agregar-contacto',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgregarContactoPage implements OnInit {
 
-  constructor() { }
+  ionicForm: FormGroup;
+  isSubmitted = false;
 
-  ngOnInit() {
+  constructor(public formBuilder: FormBuilder) {
+
+   }
+
+   ngOnInit() {
+    this.ionicForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(2)]],
+      apellido: ['', [Validators.required, Validators.minLength(2)]],
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+      mobile: ['', [Validators.required, Validators.pattern('^[0-9]+$')]]
+    })
+  }
+
+  getDate(e) {
+    let date = new Date(e.target.value).toISOString().substring(0, 10);
+    this.ionicForm.get('dob').setValue(date, {
+      onlyself: true
+    })
+  }
+ 
+
+  get errorControl() {
+    return this.ionicForm.controls;
+  }
+
+  submitForm() {
+    this.isSubmitted = true;
+    if (!this.ionicForm.valid) {
+      console.log('Please provide all the required values!')
+      return false;
+    } else {
+      console.log(this.ionicForm.value)
+    }
   }
 
 }
